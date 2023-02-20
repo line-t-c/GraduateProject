3545adc5a05cd8272fd31b78cf796b93c6e209e3
line-t-c <line_t_c@hotmail.com>
2023-02-20 14:55:10 +0100

    Commit tests

--- src/main/java/RuneLine/GraduateProject/Recipe.java
+++ src/main/java/RuneLine/GraduateProject/Recipe.java
@@ -1,6 +1,9 @@
 package RuneLine.GraduateProject;
 
-import javax.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Recipe {
--- src/main/java/RuneLine/GraduateProject/RecipeController.java
+++ src/main/java/RuneLine/GraduateProject/RecipeController.java
@@ -1,4 +1,11 @@
 package RuneLine.GraduateProject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class RecipeController {
+
+    @Autowired
+    private RecipeRepo repository;
 }
