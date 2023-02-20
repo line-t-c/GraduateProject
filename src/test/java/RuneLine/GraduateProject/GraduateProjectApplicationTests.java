af1cd539e1890b77c1348b0fbc3c1b548ad695b6
line-t-c <line_t_c@hotmail.com>
2023-02-20 14:57:53 +0100

    Creating project

--- src/test/java/RuneLine/GraduateProject/GraduateProjectApplicationTests.java
+++ src/test/java/RuneLine/GraduateProject/GraduateProjectApplicationTests.java
@@ -11,34 +11,4 @@ class GraduateProjectApplicationTests {
 	void contextLoads() {
 	}
 
-
-
-//	Test for hvad?
-//	Arrange. Act. Assert. De tre linjer i eks
-//	Kan lave en
-
-//	HelloService sut;
-//	@BeforeEach
-//	public void before() {
-//    sut = new HelloService();
-//	}
-//	@Test
-//	public void testHelloService() {
-//    //HelloService sut = new HelloService(); -- denne udelades da
-//    String message = sut.getMessage(null);
-//}
-
-//	@Test
-//	public void testHelloService() {
-//		GraduateProjectApplication.HelloService sut = new GraduateProjectApplication.HelloService();
-//		String message = sut.getMessage("Academy");
-//		Assertions.assertEquals("Hello Academy", message);
-//	}
-
-//	public class HelloService {
-//    public String getMessage(String name) {
-//        return "Hello " + name;
-//    }
-//}
-
 }
