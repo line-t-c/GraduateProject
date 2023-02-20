7f3c697045006bfdf604b66dbd840a09b1775766
line-t-c <line_t_c@hotmail.com>
2023-02-20 16:14:01 +0100

    Creating project

--- src/main/java/RuneLine/GraduateProject/Recipe.java
+++ src/main/java/RuneLine/GraduateProject/Recipe.java
@@ -1,9 +1,6 @@
 package RuneLine.GraduateProject;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Recipe {
@@ -12,11 +9,48 @@ public class Recipe {
     private Long id;
 
     private String title;
-    private String instruction;
+    private Instruction instruction;
     private Ingredient ingredient;
-//    private String author;
 
+    public Recipe() {
+    }
 
+    public Recipe(Long id, String title, Instruction instruction, Ingredient ingredient) {
+        this.id = id;
+        this.title = title;
+        this.instruction = instruction;
+        this.ingredient = ingredient;
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instruction getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(Instruction instruction) {
+        this.instruction = instruction;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
 }
