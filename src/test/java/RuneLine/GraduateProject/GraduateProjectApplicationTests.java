1c76c61bb40ba99806a3e1aa89aae1882d20e572
line-t-c <line_t_c@hotmail.com>
2023-02-21 08:24:59 +0100

    Creating tests

--- src/test/java/RuneLine/GraduateProject/GraduateProjectApplicationTests.java
+++ src/test/java/RuneLine/GraduateProject/GraduateProjectApplicationTests.java
@@ -1,6 +1,7 @@
 package RuneLine.GraduateProject;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,6 +11,27 @@ class GraduateProjectApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	Recipe sut;
+	@BeforeEach
+	public void before() {
+		sut = new Recipe();
+	}
+
+//	@Test
+//	public void test1() {
+//	String message = sut.getMessage("Academy");
+//	Assertions.assertEquals("Hello Academy", message);
+//	}
+
+//	Metoder:
+//	- Search Ingredients
+//	- Filter recipes (fx Allergener / diet)
+//	- Choose recipe
+//	- Change potion size --> alter ingredient amount
+//	- Current step --> Next step (instructions)
+//	- Sæson, vise grøntsager i en sæson (tags)
+
 
 
 
