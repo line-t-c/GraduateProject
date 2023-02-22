45375a5a52d3543102eeb087148d35221709238c
line-t-c <line_t_c@hotmail.com>
2023-02-22 16:16:10 +0100

    Creating sql

--- src/main/java/RuneLine/GraduateProject/RecipeController.java
+++ src/main/java/RuneLine/GraduateProject/RecipeController.java
@@ -3,10 +3,9 @@ package RuneLine.GraduateProject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class RecipeController {
@@ -43,6 +42,60 @@ public class RecipeController {
     public String searchResult () {
         return "result";
     }
+
+//    @GetMapping("/search")
+//    public String searchRecipesByIngredient(@RequestParam String ingredients, Model model) {
+//        List<Recipe> recipes = repository.findByIngredientName(ingredients);
+//        model.addAttribute("recipes", recipes);
+//        return "searchResults" + ingredients;
+//    }
+
+    @GetMapping("/search")
+    public String searchRecipesByIngredient(@RequestParam(name = "ingredient_name", required = false) String ingredientName, Model model) {
+        if (ingredientName == null || ingredientName.isEmpty()) {
+            // handle the case where no ingredient name is provided
+        } else {
+            List<Recipe> recipes = repository.findByIngredientName(ingredientName);
+            model.addAttribute("recipes", recipes);
+            model.addAttribute("ingredientName", ingredientName);
+            return "searchResults";
+        }
+        return "searchResults";
+    }
+
+
+
+
+//    @GetMapping("/search")
+//    public String searchRecipesByIngredient(@RequestParam(name = "ingredient_name") String ingredientName, Model model) {
+//        List<Recipe> recipes = repository.findByIngredientName(ingredientName);
+//        model.addAttribute("recipes", recipes);
+//        return "searchResults" + ingredientName;
+//    }
+
+
+//    @GetMapping("/search/{ingredient_name}")
+//    public String searchRecipesByIngredient(@PathVariable String ingredient_name, Model model) {
+//        List<Recipe> recipes = repository.findByIngredientName(ingredient_name);
+//        model.addAttribute("recipes", recipes);
+//        return "searchResults" + ingredient_name;
+//    }
+
+//    http://localhost:8080/search?ingredients=bacon
+
+//                for (Recipe recipe : recipes) {
+//            String imageUrl = recipe.getImageUrl();
+//            model.addAttribute("imageUrl", imageUrl);
+//            }
+
+
+
+//    @GetMapping("/search")
+//    public String searchRecipesByIngredients(@RequestParam("ingredients") String ingredients, Model model) {
+//        List<Recipe> recipes = repository.findRecipesByIngredients(ingredients.split(","));
+//        model.addAttribute("recipes", recipes);
+//        return "searchResults";
+//    }
 
 //    @PostMapping("/result")
 //    public String searchResul () {
--- src/main/java/RuneLine/GraduateProject/RecipeRepo.java
+++ src/main/java/RuneLine/GraduateProject/RecipeRepo.java
@@ -1,11 +1,39 @@
 package RuneLine.GraduateProject;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface RecipeRepo extends CrudRepository<Recipe, Long> {
+
+//    @Query("SELECT r FROM Recipe r JOIN r.ingredients i WHERE i.ingredientName IN :ingredients")
+//    List<Recipe> findRecipesByIngredients(@Param("ingredients") String[] ingredients);
+//
+//    @Query("SELECT r FROM Recipe r JOIN r.ingredients i WHERE i.ingredient_name = ?1")
+//    List<Recipe> findByIngredient(String ingredientName);
+
+//    Error interpreting query [SELECT DISTINCT r FROM Recipe r JOIN r.ingredients i WHERE i.ingredientname = ?1];
+//    this may indicate a semantic (user query) problem or a bug in the parser
+//    [SELECT DISTINCT r FROM Recipe r JOIN r.ingredients i WHERE i.ingredientname = ?1]
+
+//    Error interpreting query
+//    [SELECT DISTINCT r FROM Recipe r JOIN r.ingredients i WHERE i.ingredientname = ?1];
+//    this may indicate a semantic (user query) problem or a bug in the parser
+//    [SELECT DISTINCT r FROM Recipe r JOIN r.ingredients i WHERE i.ingredientname = ?1]
+
+//    Could not resolve attribute 'ingredientname' of 'RuneLine.GraduateProject.Ingredient'
+        @Query("SELECT DISTINCT r FROM Recipe r JOIN r.ingredients i WHERE i.ingredient_name = ?1")
+        List<Recipe> findByIngredientName(String ingredientName);
+
+
+
+
 
 //    A class that implements this interface and it’s methods (inherited from CrudRepository) will be created
 //automaticallyl
