dcc4e432847710cf736b7333f17ec8adacff6925
line-t-c <line_t_c@hotmail.com>
2023-03-01 22:18:18 +0100

    Updated home.html

--- src/main/java/RuneLine/GraduateProject/RecipeRepo.java
+++ src/main/java/RuneLine/GraduateProject/RecipeRepo.java
@@ -14,5 +14,4 @@ public interface RecipeRepo extends CrudRepository<Recipe, Long> {
     @Query("SELECT r FROM Recipe r WHERE r.id IN :id")
     List<Recipe> findRecipesById(@Param("id") List<Long> id);
 
-
 }
