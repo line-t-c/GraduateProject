766c64927e2c352b7fb0b1218d05a9dce39874d5
line-t-c <line_t_c@hotmail.com>
2023-03-02 09:15:58 +0100

    Updated data.sql

--- src/main/java/RuneLine/GraduateProject/RecipeController.java
+++ src/main/java/RuneLine/GraduateProject/RecipeController.java
@@ -1,6 +1,5 @@
 package RuneLine.GraduateProject;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/RuneLine/GraduateProject/RecipeRepo.java
+++ src/main/java/RuneLine/GraduateProject/RecipeRepo.java
@@ -1,6 +1,5 @@
 package RuneLine.GraduateProject;
 
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
